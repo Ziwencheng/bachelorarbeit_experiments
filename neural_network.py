2146148ef4fe77546e0cc0ec57d5ed653961d3f1
ziwencheng <chengziwen693@gmail.com>
2022-07-04 10:07:37 +0200

    Add Tuning

--- neural_network.py
+++ neural_network.py
@@ -1,7 +1,24 @@
 import torch
 import torch.nn as nn
+#from torch.nn import functional as F
 
 class Neural_Network(nn.Module):
+    def __init__(self, input_size, num_layers, layer_size, output_size):
+        super(Neural_Network, self).__init__()
+        self.layers = nn.ModuleList()
+        self.tanh = nn.Tanh()
+        self.layers.append(nn.Linear(input_size, layer_size))
+        for i in range(1, num_layers - 1):
+            self.layers.append(nn.Linear(layer_size, layer_size))
+        self.layers.append(nn.Linear(layer_size, output_size))
+
+    def forward(self, x):
+        y = x
+        for i in range(len(self.layers)):
+            y = self.layers[i](y)
+            y = self.tanh(y)
+        return y
+    """
     def __init__(self, input_dim, hidden_dim, output_dim):
         super().__init__()
         # first module
@@ -19,3 +36,4 @@ class Neural_Network(nn.Module):
         x = self.linear2(x)
         x = self.tanh2(x)
         return x
+    """
--- neural_network_experiment.py
+++ neural_network_experiment.py
@@ -4,6 +4,7 @@ import dataset
 from dataloader import LIBSVMLoader
 import numpy as np
 from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 import random
 import math
 #import wandb
@@ -15,11 +16,18 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader,TensorDataset,random_split,SubsetRandomSampler, ConcatDataset
 from torch.nn import functional as F
 
+import ray
+from ray import tune
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune.suggest.bayesopt import BayesOptSearch
+import os
+
 #wandb.init(project="my-test-project", entity="ziwencheng")
 
-def get_model(lr,input_dim, hidden_dim, output_dim):
-    model = neural_network.Neural_Network(input_dim, hidden_dim, output_dim)
-    return model, optim.SGD(model.parameters(), lr=lr)
+def get_model(lr,input_dim, num_layers, hidden_dim, output_dim):
+    model = neural_network.Neural_Network(input_dim, num_layers, hidden_dim, output_dim)
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    return model, optimizer
 
 def loss_batch(model, loss_func, xb, yb, opt=None):
     pred = model(xb.float())
@@ -33,7 +41,7 @@ def loss_batch(model, loss_func, xb, yb, opt=None):
 
     return loss.item(), pred_indices
 
-def fit(model, loss_func, opt, train_dl, test_dl):
+def fit(model, loss_func, opt, train_dl, val_dl):
     #for epoch in range(epochs):
         train_loss, train_correct = 0.0, 0
         model.train()
@@ -50,7 +58,7 @@ def fit(model, loss_func, opt, train_dl, test_dl):
         valid_loss, val_correct = 0.0, 0
         model.eval()
         with torch.no_grad():
-            for xb, yb in test_dl:
+            for xb, yb in val_dl:
                 val_loss, valpred_indices = loss_batch(model, loss_func, xb, yb)
                 valid_loss += val_loss * len(xb)
                 valyb_indices = torch.argmax(yb, dim=1)
@@ -61,10 +69,15 @@ def fit(model, loss_func, opt, train_dl, test_dl):
 
 def getTensorDataset(name):
     x, y = dataset.get_precise_data(name)
-    xt = torch.from_numpy(x)
-    yt = torch.from_numpy(y)
-    name_ds = TensorDataset(xt, yt)
-    return name_ds
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    xt_train = torch.from_numpy(x_train)
+    xt_test = torch.from_numpy(x_test)
+    yt_train = torch.from_numpy(y_train)
+    yt_test = torch.from_numpy(y_test)
+    #name_ds = TensorDataset(xt, yt)
+    nameTrain_ds = TensorDataset(xt_train, yt_train)
+    nameTest_ds = TensorDataset(xt_test, yt_test)
+    return nameTrain_ds, nameTest_ds
 
 def getTensorImpreciseDataset(name, corruption):
     x, y = dataset.get_imprecise_data(name, corruption)
@@ -119,15 +132,133 @@ def AC_CrossEntropy(pred, yb):
 
 ce_loss_func = F.cross_entropy
 
-svmguide2_ds = getTensorDataset('svmguide2')
+
 dna_ds = getTensorDataset('dna')
 vowel_ds = getTensorDataset('vowel')
 segment_ds = getTensorDataset('segment')
 
 x, y = dataset.get_imprecise_data('svmguide2', 0.3)
-#print(x, y)
 svmguide2_ids = getTensorImpreciseDataset('svmguide2', 0.3)
 
+search_space = {
+    "lr": tune.sample_from(lambda spec: 10 ** (-10 * np.random.rand())),
+    "hidden_dim": tune.randint(5, 10),
+    "num_layers": tune.randint(2, 5)
+}
+
+algo = BayesOptSearch(random_search_steps=4)
+
+def train(config, k, epochs, input_dim, output_dim, batch_size, loss_func, name, checkpoint_dir=None) -> None:
+    train_ds, test_ds = getTensorDataset(name)
+    #foldperf = {}
+
+    model, opt = get_model(config["lr"], input_dim, config["num_layers"], config["hidden_dim"], output_dim)
+
+    if checkpoint_dir:
+        checkpoint = os.path.join(checkpoint_dir, "checkpoint")
+        model_state, optimizer_state = torch.load(checkpoint)
+        model.load_state_dict(model_state)
+        opt.load_state_dict(optimizer_state)
+    for epoch in range(epochs):
+        vall_f, trainl_f = [], []
+        splits = KFold(n_splits=k, shuffle=True, random_state=42)
+        for fold, (train_idx, val_idx) in enumerate(splits.split(np.arange(len(train_ds)))):
+
+            train_sampler = SubsetRandomSampler(train_idx)
+            val_sampler = SubsetRandomSampler(val_idx)
+            train_loader = DataLoader(train_ds, batch_size=batch_size, sampler=train_sampler)
+            val_loader = DataLoader(train_ds, batch_size=batch_size, sampler=val_sampler)
+
+            #history = {'train_loss': [], 'val_loss': [], 'train_acc': [], 'test_acc': []}
+
+            train_loss, train_correct, val_loss, val_correct = fit(model, loss_func, opt, train_loader, val_loader)
+
+            train_loss = train_loss / len(train_loader.sampler)
+            # train_acc = train_correct / len(train_loader.sampler) * 100
+            val_loss = val_loss / len(val_loader.sampler)
+            # val_acc = val_correct / len(val_loader.sampler) * 100
+
+            vall_f.append(train_loss)
+            trainl_f.append(val_loss)
+
+            #history['train_loss'].append(train_loss)
+            #history['val_loss'].append(val_loss)
+            # history['train_acc'].append(train_acc)
+            # history['test_acc'].append(val_acc)
+
+            #foldperf['fold{}'.format(fold + 1)]['train_loss'] = train_loss
+            #foldperf['fold{}'.format(fold + 1)]['val_loss'] = val_loss
+
+            #vall_f, trainl_f, vala_f, traina_f = [], [], [], []
+            """
+            for f in range(1, k + 1):
+                trainl_f.append(foldperf['fold{}'.format(f)]['train_loss'])
+                vall_f.append(foldperf['fold{}'.format(f)]['val_loss'])
+
+                # traina_f.append(np.mean(foldperf['fold{}'.format(f)]['train_acc']))
+                # testa_f.append(np.mean(foldperf['fold{}'.format(f)]['test_acc']))
+            """
+        with tune.checkpoint_dir(step=epoch) as checkpoint_dir:
+            path = os.path.join(checkpoint_dir, "checkpoint")
+            torch.save(
+                (model.state_dict(), opt.state_dict()), path)
+
+        tune.report(loss=np.mean(vall_f))
+    print("Finished Training")
+
+def test_best_model(best_trial, input_dim, output_dim, name, batch_size, loss_func):
+    model, opt = get_model(best_trial.config["lr"], input_dim, best_trial.config["num_layers"],
+                           best_trial.config["hidden_dim"], output_dim)
+
+    checkpoint_path = os.path.join(best_trial.checkpoint.value, "checkpoint")
+
+    model_state, optimizer_state = torch.load(checkpoint_path)
+    model.load_state_dict(model_state)
+
+    train_ds, test_ds = getTensorDataset(name)
+
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+
+    train_loss, train_correct, test_loss, test_correct = fit(model, loss_func, opt, train_loader, test_loader)
+
+    print("Best trial test set accuracy: {}".format(test_correct/len(test_ds) * 100))
+
+def main(k, epochs, input_dim, output_dim, batch_size, loss_func, name, num_samples=10, max_num_epochs=10):
+    search_space = {
+        "lr": tune.sample_from(lambda spec: 10 ** (-10 * np.random.rand())),
+        "hidden_dim": tune.randint(5, 10),
+        "num_layers": tune.randint(2, 5)
+    }
+    scheduler = ASHAScheduler(
+        max_t=max_num_epochs,
+        grace_period=1,
+        reduction_factor=2)
+    analysis = tune.run(
+        tune.with_parameters(train, k=k, epochs=epochs, input_dim=input_dim, output_dim=output_dim,
+                             batch_size=batch_size, loss_func=loss_func, name=name),
+        config=search_space,
+        metric="loss",
+        mode="min",
+        # search_alg=algo,
+        # stop={"training_iteration": 20},
+        num_samples=num_samples,
+        scheduler=scheduler
+    )
+
+    best_trial = analysis.get_best_trial("loss", "min", "last")
+    print("Best trial config: {}".format(best_trial.config))
+    print("Best trial final validation loss: {}".format(
+        best_trial.last_result["loss"]))
+    #print("Best trial final validation accuracy: {}".format(
+        #best_trial.last_result["accuracy"]))
+
+    if ray.util.client.ray.is_connected():
+        from ray.util.ml_utils.node import force_on_current_node
+        remote_fn = force_on_current_node(ray.remote(test_best_model))
+        ray.get(remote_fn.remote(best_trial))
+    else:
+        test_best_model(best_trial, input_dim, output_dim, name, batch_size, loss_func)
 
 def run(k, lr, batch_size, epochs, loss_func, input_dim, hidden_dim, output_dim, ds) -> None:
     foldperf = {}
@@ -190,11 +321,24 @@ def run(k, lr, batch_size, epochs, loss_func, input_dim, hidden_dim, output_dim,
 #run(10, 0.1, 32, 10, ce_loss_func, 180, 5, 3, dna_ds)
 #run(10, 0.1, 8, 10, ce_loss_func, 10, 5, 11, vowel_ds)
 #run(10, 0.1, 32, 10, ce_loss_func, 19, 5, 7, segment_ds)
-run(10, 0.1, 8, 3, OSL_CrossEntropy, 20, 5, 3, svmguide2_ids) # overfitting occurs after 3 epochs
-run(10, 0.1, 8, 3, AC_CrossEntropy, 20, 5, 3, svmguide2_ids)
-
-
-
-
-
-
+#run(10, 0.1, 8, 3, OSL_CrossEntropy, 20, 5, 3, svmguide2_ids) # overfitting occurs after 3 epochs
+#run(10, 0.1, 8, 3, AC_CrossEntropy, 20, 5, 3, svmguide2_ids)
+#analysis = tune.run(train(10, search_space, 10, 20, 3, 8, ce_loss_func, 'svmguide2'), config=search_space)
+"""
+analysis2 = tune.run(
+    tune.with_parameters(train, k=10, epochs=10, input_dim=180, output_dim=3, batch_size=32, loss_func=ce_loss_func,
+                         name='dna'),
+    config=search_space,
+    mode="max",
+    metric="mean_accuracy",
+    #search_alg=algo,
+    #stop={"training_iteration": 20},
+    num_samples=10
+)
+best_config = analysis.best_config
+#print("best config: ", analysis.get_best_config(metric="mean_accuracy", mode="max"))
+#print("best config: ", analysis2.get_best_config(metric="mean_accuracy", mode="max"))
+#print(best_config)
+"""
+main(10, 10, 20, 3, 8, ce_loss_func, "svmguide2")
+#k, epochs, input_dim, output_dim, batch_size, loss_func, name
